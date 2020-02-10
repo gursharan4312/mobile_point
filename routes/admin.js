3cff71e48d4581a6c355dc2bffb288d6328f09d1
Gursharan Singh <gursharansingh4312@gmail.com>
2020-02-10 13:54:36 -0800

    rm

--- routes/admin.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const { loginAdmin, postAdmin } = require("../controllers/adminController.js");
-
-router.post("/login", loginAdmin);
-router.post("/", postAdmin);
-
-const ordersRouter = router;
-
-module.exports = ordersRouter;
